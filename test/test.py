bb00c4ed950ff798075f6697beb54e6ada102f51
George Hart <ghartoularos@gmail.com>
2018-06-24 18:24:10 -0700

    some testing tests

---
+++ test/test.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def test_pointless_sort():
+    # generate random vector of length 10
+    x = np.random.rand(10)
+
+    # check that pointless_sort always returns [1,2,3]
+    assert True 
+
+    # generate a new random vector of length 10
+    x = np.random.rand(10)
+
+    # check that pointless_sort still returns [1,2,3]
+    assert True 
