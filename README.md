c88bc12d43a624c2ba93dc475d4c2c5eddc22c42
George Hart <ghartoularos@gmail.com>
2018-06-27 18:56:22 -0400

    timer and time complexity plotting

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 [![Build Status](https://travis-ci.org/ghartoularos/hw1.svg?branch=master)](https://travis-ci.org/ghartoularos/hw1)
 
-Repository for Algorithms HW1.
+###Repository for Algorithms HW1.
+Note timing and plotting scripts require additional packages not explicitly listed in requirements.txt file:
+* matplotlib
+* tqdm
+* timeit
+* pickle
--- test/test.py
+++ test/test.py
@@ -1,14 +1,19 @@
-import numpy as np
+from algs import bubblesort, quicksort
 
-def test_pointless_sort():
-    # generate random vector of length 10
-    x = np.random.rand(10)
+def testing():
+    # generate random integers, repeats
+    x1 = [4,2,5,3,4]
+    y1 = [2,3,4,4,5]
 
-    # check that pointless_sort always returns [1,2,3]
-    assert True 
+    # negatives
+    x2 = [-4,10,23,-2,6]
+    y2 = [-4,-2,6,10,23]
 
-    # generate a new random vector of length 10
-    x = np.random.rand(10)
+    # decimals, repeats
+    x3 = [-4.2,10.7,10.3,9.9,9.9]
+    y3 = [-4.2,9.9,9.9,10.3,10.7]
 
-    # check that pointless_sort still returns [1,2,3]
-    assert True 
+    for i, j in ((x1,y1),(x2,y2),(x3,y3)):
+        assert list(bubblesort(i)) == j
+    for i, j in ((x1,y1),(x2,y2),(x3,y3)):
+        assert list(quicksort(i)) == j
